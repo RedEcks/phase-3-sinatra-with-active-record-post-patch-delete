d0dd9dd36d0cc8d3dc9efd3fa715a117bbb49236
Xavier Dunkley <xavierxv18@gmail.com>
2023-03-09 14:55:20 -0500

    Completed lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,31 @@ class ApplicationController < Sinatra::Base
     games.to_json
   end
 
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
+
   get '/games/:id' do
     game = Game.find(params[:id])
 
